e3384090bacca929978112734abcf3a05c67dc29
asdf <doorumi123@gmail.com>
2019-06-20 15:46:48 +0900

    project_insert
    query insert

--- api/auth.js
+++ api/auth.js
@@ -26,18 +26,80 @@ var connection = mysql.createConnection({
 //test
 router.post('/', function(req,res,next){ //all --> post
   data = req.body;
+  var name='';
   console.log('name is :'+data.name);
+
+  connection.escape();
+  var query=connection.query('select project_name from project where project_name=?', data.name, function(err, results){
+    if (err) {
+     console.log(err);
+   }
+   console.log(results[0]);
+   console.log(typeof(results[0]));
+   if(!results[0]){
+    var query=connection.query('insert into project values((select max(project_id)+1 from project a),?)',data.name, function (err, results) {
+      if (err) {
+       console.log(err);
+     }
+     console.log('name3 ='+data.name);
+     return res.json({message:"name is insert!"});
+   });
+  }
+  else{
+   name=results[0].project_name;
+   console.log('result = '+results[0].project_name);
+   console.log('name = '+name);
+   if(name==data.name){
+    console.log('name2 ='+name);
+    return res.json({message:"name is duplicated!"});
+    }
+  }
+  //특이 사항 변수 쿼리가 아닐 경우 에러 발생
+});
+  // var query1=connection.query('insert into project values((select max(project_id)+1 from project a),?)',data.name, function (err, results) {
+  //     if (err) {
+  //      console.log(err);
+  //    }
+  //    console.log('name3 ='+data.name);
+  //    return res.send('name3 ='+data.name);
+  //  });
   // output message
-  connection.query('SELECT * FROM project', function (error, results, fields) {
-      if (error) {
-          console.log(error);
-      }
-      console.log(results);
-      res.json({result:results});
-  });
+  // insert_into_query(data.name,res);
+  
+  // connection.query(`insert into project values((select count(project_id)+1 from project a),'${data.name}')`, function (error, results, fields) {
+  //   if (error) {
+  //     console.log(error);
+  //   }
+  //   console.log(results);
+  //   res.json({result:results});
+  // });
 });
 
 /////////////////////////////////////////////////////////////////////////////////////////
+
+function insert_into_query(data,res) {
+  var name='';
+  console.log(data);
+  connection.escape();
+  var query=connection.query('select project_name from project where project_name=?', data, function(err, results){
+    if (err) {
+      console.log(err);
+    }
+    name=results[0].project_name;
+    console.log('result = '+results[0].project_name);
+    console.log('name = '+name);
+  });
+  console.log('name1 ='+name);
+  console.log('data ='+data);
+  if(name==data){
+    res.send('name2 ='+name);
+  }
+  else{
+    console.log('name3');
+    res.json({result:results});
+  }
+}
+
 
 // login
 router.post('/login',
